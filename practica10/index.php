83c84ae26d2921712f3e9acf85eedb606bfe92c8
Jose-Gildardo <j_gildardo@hotmail.es>
2017-03-13 22:06:15 -0600

    Add files via upload

---
+++ practica10/index.php
@@ -0,0 +1,11 @@
+<?php
+echo "hola mundo php";
+?>
+<html>
+<dead>
+<titele>practica 10 php</title>
+</head>
+<body>
+<h1> Hola mundo html </h1>
+</body>
+</html>
---
+++ practica10/index2.php
@@ -0,0 +1,16 @@
+<html>
+<dead>
+<titele>practica 10 php</title>
+</head>
+<body>
+<h1> usando PHP</h1>
+	<p><?php
+	echo "parrafo php";
+	?></p>
+	<p>Parrafo HTMl</p>
+	<h3><?php
+	echo "parrafo php";
+	?>
+	</h3>
+</body>
+</html>
